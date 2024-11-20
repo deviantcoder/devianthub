0e7d63ab988e32e88c5b0c676132d100105c17ac
deviantcoder <andrewsafonovpost@gmail.com>
2024-11-20 18:14:01 +0200

    commit 33

--- static/js/main.js
+++ static/js/main.js
@@ -11,15 +11,15 @@ document.addEventListener('DOMContentLoaded', () => {
     //         });
     //     });
     // }
-    const burger = document.querySelector('.navbar-burger');
-    const menu = document.querySelector('.navbar-menu');
+    // const burger = document.querySelector('.navbar-burger');
+    // const menu = document.querySelector('.navbar-menu');
 
-    if (burger && menu) {
-        burger.addEventListener('click', () => {
-            burger.classList.toggle('is-active');
-            menu.classList.toggle('is-active');
-        });
-    }
+    // if (burger && menu) {
+    //     burger.addEventListener('click', () => {
+    //         burger.classList.toggle('is-active');
+    //         menu.classList.toggle('is-active');
+    //     });
+    // }
 });
 
 // Toggle description function
